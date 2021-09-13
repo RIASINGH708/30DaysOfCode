ef1e36ab6d3cb2772120be27958b999d966ac62c
RIYA KUMARI <RIASINGH708@GMAIL.COM>
2021-09-13 14:02:31 +0530

    Day 24: More Linked Lists

---
+++ day24.cpp
@@ -0,0 +1,48 @@
+#include <cstddef>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+          Node* removeDuplicates(Node *head)
+          {
+            //Write your code here
+              if(head == nullptr){
+                  return head;
+              }
+              
+              Node *currentNode = head, *lastNode = head;
+              int lastVal = head->data;
+              for(;;){
+                  currentNode = currentNode->next;
+                  if(currentNode == nullptr){
+                      break;
+                  }
+                  
+                  int currentVal = currentNode->data;
+                  if(currentVal == lastVal){
+                      lastNode->next = currentNode->next;
+                      currentNode = lastNode;
+                  } else {
+                    lastVal = currentVal;
+                    lastNode = currentNode;
+                  }
+              }             
+              return head;              
+          }
+
+          Node* insert(Node *head,int data)
